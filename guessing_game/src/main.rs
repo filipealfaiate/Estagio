c1b2f1ee418d5d591e8d0ac2fc8da8d5e670b573
filipealfaiate <filalfa1.fa@gmail.com>
2021-03-04 20:02:21 +0000

    guessing game terminado e comeco do estudo do capitulo 3

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,30 +8,27 @@ fn main()
 
 	let secret_number = rand::thread_rng().gen_range(0, 101);
 
-	println!("The secret number is: {}", secret_number);
+	loop
+	{
+		println!("Please input your guess:");
 
-	println!("Please input your guess:");
+		let mut input = String::new();
+		//mut -> significa que a variavel pode mudar
+		// sem mut significa que é estática
+		//new() é um método da String que cria uma nova String vazia
 
-	let mut input = String::new();
-	//mut -> significa que a variavel pode mudar
-	// sem mut significa que é estática
+		io::stdin().read_line(&mut input)
+			.expect("failed");
 
-	//new() é um método da String que cria uma nova String vazia
+		let input: u32 = match input.trim().parse()
+							{ Ok(num) => num, Err(_) => continue,};
 
-	let input: u32 = input.trim().parse().expect("Please type a number!");
+		println!("You guessed: {}", input);
 
-
-	io::stdin().read_line(&mut input)
-		.expect("failed");
-
-	println!("You guessed: {}", input);
-
-	match input.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
-
+		match input.cmp(&secret_number) {
+	        Ordering::Less => println!("Too small!"),
+	        Ordering::Greater => println!("Too big!"),
+	        Ordering::Equal => {println!("You win!"); break;}
+	    }
+	 }
 }
-
-//Rust Enhanced
